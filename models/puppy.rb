1a6c31298744e9e668cd6b954d8967d39f91a4a6
Matthew C <m23.cullen@gmail.com>
2019-10-24 22:44:41 +0000

    Automatically backed up by Learn

---
+++ models/puppy.rb
@@ -0,0 +1,11 @@
+class Puppy 
+  attr_accessor :name, :breed, :months_old
+  @@all = []
+  
+    def initialize(name,breed,age)
+      @name = name 
+      @breed = breed 
+      @months_old = age
+      @@all << self 
+    end 
+end 
